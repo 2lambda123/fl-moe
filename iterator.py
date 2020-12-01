2c1961330539d53067e78fa9d849fd7918b66ba7
Martin Isaksson <martin.isaksson@ericsson.com>
2020-12-01 10:59:36 +0100

    Update iterator

--- iterator.py
+++ iterator.py
@@ -2,34 +2,56 @@ import subprocess
 import json
 from utils.util import read_config, get_logger
 import numpy as np
+import argparse
 
-#list_files = subprocess.run(["ls", "-l"])
-#print("The exit code was: %d" % list_files.returncode)
 
-mylogger = get_logger("Iterator")
+def args_parser():
+    parser = argparse.ArgumentParser()
 
-config = read_config("config.json")
+    parser.add_argument(
+        '--filename',
+        default=[], help='configuration filename',
+        action="append")
+    parser.add_argument('--dry-run', action='store_true', help='do not fire')
+    return parser.parse_args()
 
-pvals = np.linspace(0,1,10+1)
-mylogger.info(f"Starting experiment with p={pvals}")
 
-child_processes = []
+if __name__ == "__main__":
 
-for n, p in enumerate(pvals):
+    args = args_parser()
+    mylogger = get_logger("Iterator")
 
-    config["p"] = p
-    config["gpu"] = n%8
-    config["filename"] = f"results_{p}.csv"
+    mylogger.debug(args)
+    # Loop over multiple files
 
-    command = ["python", "main_fed.py"]
+    for filename in args.filename:
+        config = read_config(filename)
 
-    for k,v  in config.items():
-        command.extend([f"--{k}", str(v)])
+        pvals = np.logspace(-1,-10,10)
+        mylogger.info(f"Starting experiment from {filename} with lr={pvals}")
 
-    mylogger.debug(command)
+        child_processes = []
 
-    p = subprocess.Popen(command)
-    child_processes.append(p)
+        # Make variable replacable
+        for n, p in enumerate(pvals):
 
-for cp in child_processes:
-    cp.wait()
+            config["lr"] = p
+            config["gpu"] = n % 8
+            dataset = config["dataset"]
+            config["filename"] = f"results_{dataset}_p_{p}.csv"
+
+            command = ["python", "main_fed.py"]
+
+            for k, v in config.items():
+                command.extend([f"--{k}", str(v)])
+
+            #command.extend(["--overlap"])
+            mylogger.debug(command)
+
+            # Allow dry-runs
+            if not args.dry_run:
+                p = subprocess.Popen(command)
+                child_processes.append(p)
+
+        for cp in child_processes:
+            cp.wait()
