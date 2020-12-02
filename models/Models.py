ad6de88d433a95c4f438ca08c9e0c8e3aeb70920
Martin Isaksson <martin.isaksson@ericsson.com>
2020-12-02 18:41:53 +0100

    Model changes for FEMNIST

--- models/Models.py
+++ models/Models.py
@@ -59,6 +59,38 @@ class GateMLP(nn.Module):
         return x
 
 
+class CNNLeafFEMNIST(nn.Module):
+    """
+    Model from LEAF paper, but with dropout
+    TODO: Implicit dimension choice for log_softmax has been deprecated
+    """
+
+    def __init__(self, args):
+
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.fc1 = nn.Linear(64 * 4 * 4, 512)
+        self.dropout = nn.Dropout()
+        self.fc2 = nn.Linear(512, args.num_classes)
+        self.activation = nn.LogSoftmax()
+
+    def forward(self, x):
+        """
+        Forward pass
+        """
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        out1 = F.relu(self.fc2(x))
+        out2 = self.activation(out1)
+        return out1, out2
+
 class CNNLeaf(nn.Module):
     """
     Model from LEAF paper, but with dropout
@@ -69,13 +101,13 @@ class CNNLeaf(nn.Module):
 
         super().__init__()
 
-        self.conv1 = nn.Conv2d(args.channels, 32, 5)
+        self.conv1 = nn.Conv2d(3, 32, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 5)
         self.fc1 = nn.Linear(64 * 5 * 5, 512)
         self.dropout = nn.Dropout()
         self.fc2 = nn.Linear(512, args.num_classes)
-        self.activation = nn.LogSoftmax(dim=args.num_classes)
+        self.activation = nn.LogSoftmax()
 
     def forward(self, x):
         """
