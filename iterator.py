fe1e3551f111c6b251bc023a005d3bdabd49d3de
Martin Isaksson <martin.isaksson@ericsson.com>
2020-12-09 23:05:44 +0100

    Update iterator

--- iterator.py
+++ iterator.py
@@ -3,6 +3,15 @@ import json
 from utils.util import read_config, get_logger
 import numpy as np
 import argparse
+import torch.cuda as cutorch
+
+def get_available_gpus(threshold=0.5):
+    """
+    TODO: Implement
+    """
+    avail_gpus = [0, 1, 2, 3, 4, 5, 6, 7]
+
+    return avail_gpus
 
 
 def args_parser():
@@ -18,31 +27,38 @@ def args_parser():
 
 if __name__ == "__main__":
 
+
+
     args = args_parser()
     mylogger = get_logger("Iterator")
 
     mylogger.debug(args)
     # Loop over multiple files
 
+    gpus = get_available_gpus()
+    number_of_gpus = len(gpus)
+    mylogger.debug(f"gpus: {gpus}")
+
     for filename in args.filename:
         config = read_config(filename)
 
-        pvals = np.logspace(-6,-2, 20)
-        mylogger.info(f"Starting experiment from {filename} with lr={pvals}")
+        # for clusters in range(1, config["clusters"] + 1  )
+        pvals = np.linspace(.2, 1, 9)
+        mylogger.info(f"Starting experiment from {filename} with p={pvals}")
 
         child_processes = []
+
+        dataset = config["dataset"]
+        model = config["model"]
 
         # Make variable replacable
         for n, p in enumerate(pvals):
 
-            config["lr"] = p
-            config["ft_lr"] = p
-            config["local_lr"] = p
-            config["moe_lr"] = p
+            config["p"] = np.round(p / .1) * .1
+            config["gpu"] = gpus[n % number_of_gpus]
+            mylogger.debug(f"Assigning p={p} to GPU {gpus[n % number_of_gpus]}")
 
-            config["gpu"] = n % 8
-            dataset = config["dataset"]
-            config["filename"] = f"results_{dataset}_lr_{p}.csv"
+            config["filename"] = f"results_{dataset}_{model}_p_{p:.2f}"
 
             command = ["python", "main_fed.py"]
 
